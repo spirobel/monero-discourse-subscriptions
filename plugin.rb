a337e0b5240087570432ab3b8891e667d7b71a73
spirobel <spirobel@protonmail.com>
2022-09-02 02:44:45 +0800

    set plugin name

--- plugin.rb
+++ plugin.rb
@@ -1,13 +1,24 @@
 # frozen_string_literal: true
 
-# name: discourse-plugin-name
+# name: monero-discourse-subscriptions
 # about: TODO
 # version: 0.0.1
 # authors: Discourse
 # url: TODO
 # required_version: 2.7.0
 
-enabled_site_setting :plugin_name_enabled
+enabled_site_setting :monero_discourse_subscriptions_enabled
 
 after_initialize do
+    module ::MoneroDiscourseSubscriptions
+        PLUGIN_NAME ||= "monero_discourse_subscription"
+    
+        class Engine < ::Rails::Engine
+          engine_name PLUGIN_NAME
+          isolate_namespace MoneroDiscourseSubscriptions
+        end
+    
+        class Error < StandardError; end
+      end
+    puts "plugin test"
 end
