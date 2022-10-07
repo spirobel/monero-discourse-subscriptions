756b0c0c991d65493ed62c1a629c66f55b5052a8
spirobel <spirobel@protonmail.com>
2022-10-07 16:46:02 +0800

    ember admin route map

---
+++ assets/javascripts/discourse/monero-discourse-subscriptions-route-map.js
@@ -0,0 +1,24 @@
+export default {
+    resource: "admin.adminPlugins",
+    path: "/plugins",
+  
+    map() {
+      this.route("monero-discourse-subscriptions", function () {
+        this.route("products", function () {
+          this.route("show", { path: "/:product-id" }, function () {
+            this.route("plans", function () {
+              this.route("show", { path: "/:plan-id" });
+            });
+          });
+        });
+  
+  
+        this.route("subscriptions");
+        this.route("payments");
+        this.route("invoices");
+        this.route("wallets");
+
+
+      });
+    },
+  };
