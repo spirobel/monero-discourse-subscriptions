b40570cd3d37d90ea8f58720778b9ae1e61225d7
spirobel <spirobel@protonmail.com>
2022-10-07 16:43:19 +0800

    admin ruby route that will return the html if we access link directly

---
+++ app/controllers/monero_subscriptions/admin_controller.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+module MoneroDiscourseSubscriptions
+    class AdminController < ::Admin::AdminController
+      def index
+        head 200
+      end
+  
+    end
+end
