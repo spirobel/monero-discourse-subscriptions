b3b540723ca676da26f8ead661acfd69c6b0a201
spirobel <spirobel@protonmail.com>
2022-10-08 15:06:38 +0800

    emberjs: wallets route controller

---
+++ assets/javascripts/discourse/controllers/admin-plugins-monero-discourse-subscriptions-wallets.js
@@ -0,0 +1,30 @@
+import Controller from "@ember/controller";
+import AdminWallet from "../models/admin-wallet";
+import { popupAjaxError } from "discourse/lib/ajax-error";
+
+export default Controller.extend({
+  creating: null,
+  actions: {
+    openWalletCreateForm() {
+      this.set("creating", true);
+    },
+    closeWalletCreateForm() {
+      this.set("creating", false);
+    },
+    createNewWallet(params) {
+      AdminWallet.save(params)
+        .then(() => {
+          this.send("closeWalletCreateForm");
+          this.send("reloadModel");
+        })
+        .catch(popupAjaxError);
+    },
+    deleteWallet(wallet) {
+      AdminWallet.destroy(wallet)
+        .then(() => {
+          this.send("reloadModel");
+        })
+        .catch(popupAjaxError);
+    },
+  },
+});
