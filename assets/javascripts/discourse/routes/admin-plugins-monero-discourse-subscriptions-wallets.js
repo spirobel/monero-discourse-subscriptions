e7ff334dfd85e7baa21995117bd4419bab48ef88
spirobel <spirobel@protonmail.com>
2022-10-09 18:51:16 +0800

    emberjs: wallet destroy and changenode actions in the wallets route

--- assets/javascripts/discourse/routes/admin-plugins-monero-discourse-subscriptions-wallets.js
+++ assets/javascripts/discourse/routes/admin-plugins-monero-discourse-subscriptions-wallets.js
@@ -1,5 +1,9 @@
 import Route from "@ember/routing/route";
 import AdminWallet from "../models/admin-wallet";
+import bootbox from "bootbox";
+import I18n from "I18n";
+import showModal from 'discourse/lib/show-modal';
+
 
 export default Route.extend({
     model(){
@@ -9,5 +13,38 @@ export default Route.extend({
         reloadModel() {
             this.refresh();
           },
+        destroyWallet(id){
+            bootbox.confirm(
+                I18n.t("monero_discourse_subscriptions.admin.wallets.operations.destroy.confirm"),
+                I18n.t("no_value"),
+                I18n.t("yes_value"),
+                (confirmed) => {
+                  if (confirmed) {
+                    AdminWallet
+                      .destroy(id)
+                      .then(() => {
+                        this.refresh();
+                      })
+                      .catch((data) =>
+                        bootbox.alert(data.jqXHR.responseJSON.errors.join("\n"))
+                      );
+                  }
+                }
+              );
+        },
+        changeNode(id){
+            showModal('change-node', {
+                model: {
+                  newNode: (serverUri) => {
+                    AdminWallet.update(id, serverUri).then(()=>{
+                        this.refresh();
+                    })
+                    .catch((data) =>
+                    bootbox.alert(data.jqXHR.responseJSON.errors.join("\n"))
+                  );
+                  },
+                }
+              });
+        }
     }
 });
