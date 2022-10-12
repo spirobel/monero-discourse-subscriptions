a0fc26bfc3c6b022511a0069b93b6b903bb9f282
spirobel <spirobel@protonmail.com>
2022-10-12 16:12:45 +0800

    fix update Product controller

--- app/controllers/monero_subscriptions/product_controller.rb
+++ app/controllers/monero_subscriptions/product_controller.rb
@@ -19,30 +19,27 @@ module MoneroDiscourseSubscriptions
               )
       end
       def update
-        params.require(:id).permit(      
-          :name,
-          :description,
-          :restoreHeight,
-          :active,
-          :position,
-          :group,
-          :monero_wallet)
-        product = MoneroProduct.find_by(params[:id])
-        render_json_dump product.update(params)
+        wallet = MoneroProduct.find_by_id(update_product_params[:id])
+        render_json_dump wallet.update(update_product_params)
       end
       def delete
         params.require(:id)
         product = MoneroProduct.find_by_id(params[:id])
         render_json_dump product.destroy
       end
-      def show
-        params.require(:id)
-        product = MoneroProduct.find_by(params[:id])
-        render_json_dump product
-      end
       
       def create_product_params
         params.permit(      
+          :name,
+          :description,
+          :active,
+          :position,
+          :group,
+          :monero_wallet)
+      end
+      def update_product_params
+        params.permit( 
+          :id,     
           :name,
           :description,
           :active,
