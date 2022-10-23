d66b7338b182192ab85b6f26caebb86ef020c30e
spirobel <spirobel@protonmail.com>
2022-10-24 00:09:05 +0800

    show message when there are no products

--- assets/javascripts/discourse/controllers/monero-products-index.js
+++ assets/javascripts/discourse/controllers/monero-products-index.js
@@ -1,10 +1,16 @@
 import Controller from "@ember/controller";
 import discourseComputed from "discourse-common/utils/decorators";
 import User from "discourse/models/user";
+import { isEmpty } from "@ember/utils";
+
 
 export default Controller.extend({
   @discourseComputed()
   isLoggedIn() {
     return User.current();
+  },
+  @discourseComputed("model")
+  noProducts(model) {
+    return isEmpty(model);
   },
 });
