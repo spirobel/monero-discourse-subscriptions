73632e9658293e90b698ce255e59d2a66f8b55ef
spirobel <spirobel@protonmail.com>
2022-10-24 00:34:28 +0800

    only show active plans for product

--- assets/javascripts/discourse/controllers/monero-products-show.js
+++ assets/javascripts/discourse/controllers/monero-products-show.js
@@ -1,6 +1,7 @@
 import Controller from "@ember/controller";
 import discourseComputed from "discourse-common/utils/decorators";
 import User from "discourse/models/user";
+import { isEmpty } from "@ember/utils";
 
 export default Controller.extend({
     queryParams: ['selectedPlanId'],
@@ -13,6 +14,14 @@ export default Controller.extend({
     @discourseComputed()
     isLoggedIn() {
       return User.current();
+    },
+    @discourseComputed("model.monero_plans")
+    active_plans(plan) {
+      return plan.filter(p=>p.active);
+    },
+    @discourseComputed("active_plans")
+    noPlans(active_plans) {
+      return isEmpty(active_plans);
     },
 
 });
