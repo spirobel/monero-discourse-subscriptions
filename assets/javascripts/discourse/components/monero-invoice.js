500f780a1c570c3fd503644c779246dc87602a2a
spirobel <spirobel@protonmail.com>
2022-11-18 23:27:12 +0800

    first shot at refresh button

--- assets/javascripts/discourse/components/monero-invoice.js
+++ assets/javascripts/discourse/components/monero-invoice.js
@@ -1,10 +1,18 @@
 import Component from "@ember/component";
+import discourseComputed from "discourse-common/utils/decorators";
+
 export default Component.extend({
   didInsertElement(){
       this.sendAction("planClick", this.selectedPlanId);
 
 },
   actions: {
-
+    refreshInvoice(invoice){
+      console.log(invoice)
+    }
+  },
+  @discourseComputed("invoice.amount_date")
+  minutesLeft(date){
+    return moment.duration(moment(date).utc().valueOf() - moment().subtract(30, 'minutes').utc().valueOf()).humanize() ;
   },
 });
