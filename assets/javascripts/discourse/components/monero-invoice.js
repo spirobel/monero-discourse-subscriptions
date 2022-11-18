71822eba2db8d632a537f437fb31c96ab0b05fcf
spirobel <spirobel@protonmail.com>
2022-11-18 23:36:13 +0800

    Fix timeleft refresh in emberjs invoice component

--- assets/javascripts/discourse/components/monero-invoice.js
+++ assets/javascripts/discourse/components/monero-invoice.js
@@ -1,18 +1,31 @@
 import Component from "@ember/component";
-import discourseComputed from "discourse-common/utils/decorators";
+import { cancel, later, next } from '@ember/runloop';
+
 
 export default Component.extend({
+  poller: null,
+  minutesLeft: null,
   didInsertElement(){
       this.sendAction("planClick", this.selectedPlanId);
+      next(this, function(){
+        this.poller = this.pollTimeLeft();
+      });
+},
+willDestroyElement() {
+  cancel(this.poller);
+},
+pollTimeLeft(){
+  return later(this,function(){
+    if(this.invoice){
+      this.set('minutesLeft',moment.duration(moment(this.invoice.amount_date).utc().valueOf() - moment().subtract(30, 'minutes').utc().valueOf()).humanize());
 
+    }
+      this.poller = this.pollTimeLeft();
+  },1000);
 },
   actions: {
     refreshInvoice(invoice){
       console.log(invoice)
     }
-  },
-  @discourseComputed("invoice.amount_date")
-  minutesLeft(date){
-    return moment.duration(moment(date).utc().valueOf() - moment().subtract(30, 'minutes').utc().valueOf()).humanize() ;
   },
 });
