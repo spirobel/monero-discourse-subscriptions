65deee62724804976edc730eac426821a952cfc3
spirobel <spirobel@protonmail.com>
2022-11-24 03:28:59 +0800

    send product and plan with the missing invoice

--- app/models/monero_subscriptions/monero_invoice.rb
+++ app/models/monero_subscriptions/monero_invoice.rb
@@ -1,8 +1,9 @@
 class MoneroInvoice < ActiveRecord::Base
   has_many :monero_payments,  -> { includes :monero_invoice }, class_name: "MoneroPayment", :foreign_key => "monero_invoice_id",inverse_of: "monero_invoice"
   has_one :monero_subscriptions
-  
   belongs_to :monero_plan
+
+  has_one :monero_product, through: :monero_plan
   belongs_to :buyer, class_name: "User"
   belongs_to :recipient, class_name: "User"
 end
