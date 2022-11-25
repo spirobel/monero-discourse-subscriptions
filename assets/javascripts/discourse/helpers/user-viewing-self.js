dee1085700bc0d092e6c92119c49df72b8d585e2
spirobel <spirobel@protonmail.com>
2022-11-25 08:25:22 +0800

    add missing component

---
+++ assets/javascripts/discourse/helpers/user-viewing-self.js
@@ -0,0 +1,12 @@
+import { registerUnbound } from "discourse-common/lib/helpers";
+import User from "discourse/models/user";
+
+export default registerUnbound("user-viewing-self", function (model) {
+  if (User.current()) {
+    return (
+      User.current().username.toLowerCase() === model.username.toLowerCase()
+    );
+  }
+
+  return false;
+});
